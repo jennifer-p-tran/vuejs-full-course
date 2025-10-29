1505aa5120aae0f539514f43f59ec5ea6999768b
Jennifer (Phuong) Tran <75240836+jennifer-p-tran@users.noreply.github.com>
2025-10-29 04:52:14 +0000

    Added components and modified Welcome.vue

---
+++ src/components/pages/Welcome.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+    <!-- The id attribute is a unique value -->
+    <section id="welcome">
+        <div class = "benefits">
+            <h2>Complete this program<br/> if you want...</h2>
+            <div>
+                <p>✅ Follow a simple program with proven results.</p>
+                <p>✅ Get git, healthy, strong and absolutely shredded.</p>
+                <p>✅ Learn more about gym, training and technique.</p>
+            </div>
+        </div>
+        <div>
+            <h3>The Training Plan</h3>
+            <p>This training plan follows a structure known as the
+                <strong>Bro Split</strong>.
+            </p>
+            <h5><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></h5>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+</style>
